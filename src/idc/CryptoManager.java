d6aa96d6042dbbbac01d73a145be783b14e6c2c5
el-indio <el-indio@el-indio.sad-hill>
2009-04-13 16:22:41 +0200

    ajout de CryptoManager

---
+++ src/idc/CryptoManager.java
@@ -0,0 +1,91 @@
+package idc;
+
+import java.util.*;
+import java.io.*;
+import javax.crypto.KeyGenerator;
+import javax.crypto.KeyGeneratorSpi;
+import java.security.*;
+import java.security.spec.*;
+import java.math.*;
+import idc.Node;
+
+public class CryptoManager {
+	private PublicKey public_key;
+	private PrivateKey private_key;
+	private Vector VectorOfPublicKey;
+	private Vector VectorOfSecretKeys;//vecteur de clefs secrète correspondant à chaque conversation privée
+	private Signature IdSign;
+
+	public CryptoManager(){
+		  File FilePub=new File("id.pub");
+		  File FilePriv=new File("id.private");
+		  
+		  try{
+			  FileOutputStream FileStreamPub=new FileOutputStream(FilePub);
+			  FileOutputStream FileStreamPriv=new FileOutputStream(FilePriv);
+			  
+			  KeyPairGenerator KeyFact=KeyPairGenerator.getInstance("RSA");
+			  KeyFact.initialize(512);
+			  KeyPair PairOfKey=KeyFact.genKeyPair();
+			  
+			  public_key=PairOfKey.getPublic();
+			  private_key=PairOfKey.getPrivate();
+	       
+		 	  byte[] PubKey=public_key.getEncoded();
+		 	  byte[] PrivKey=private_key.getEncoded();
+		  
+		 	  if(!FilePub.isFile()){
+		 		  if(FilePub.createNewFile()){
+		 			  FileStreamPub.write(PubKey);  
+		 		  }
+		 	  }
+		  
+		  
+		  if(!FilePriv.isFile()){
+			  if(FilePriv.createNewFile()){
+				FileStreamPriv.write(PrivKey);  
+			  }
+		  }
+		  
+		  //------------- AUTHENTIFICATION ---------------------
+	        
+	       IdSign = Signature.getInstance("SHA1withRSA");
+	       IdSign.initSign(private_key);
+	        
+	        /* Update and sign the data */
+	        //IdSign.update(data);
+	        byte[] sig = IdSign.sign();
+
+	        IdSign.initVerify(public_key);
+
+	        /* Update and verify the data */
+	        //IdSign.update(data);
+	        boolean verifies = IdSign.verify(sig);
+	        
+	        
+	        System.out.println("signature verifies: " + verifies);
+	        
+	        }
+	        catch(SignatureException err){
+	        	System.out.println(err);
+	        }
+	        catch(NoSuchAlgorithmException  err){
+	        	System.out.println(err);
+	        }
+	        catch(Exception err){
+	        	System.out.println(err);
+	        }
+	        
+	        integrity();
+	        
+	    }
+		    
+		public Message decode(Message msg){
+			integrity();
+		    return msg;
+		  }	
+		
+		private void integrity(){
+		  
+		}	
+}
